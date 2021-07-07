e55e5c0860cae8d081a3822d7e82f20beaba7868
archepro84 <archepro84@gmail.com>
2021-07-07 23:45:05 +0900

    post modify Update
    게시글 수정 기능을 추가하였습니다.
    수정 버튼의 출력 유무를 userId를 추출해 현재 Token의 값과 비교하여 같을 경우에만 btn.show()를 하도록 설정하였습니다.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router_comment = require("./routers/router_comment")
 const router_write = require("./routers/router_write")
 const router_login = require("./routers/router_login")
 const router_sign = require("./routers/router_sign")
+const router_modify = require("./routers/router_modify")
 const nunjucks = require("nunjucks");
 
 const app = express();
@@ -33,6 +34,8 @@ app.use("/comment", express.urlencoded({extended: false}), router_comment)
 app.use("/write", express.urlencoded({extended: false}), router_write)
 app.use("/sign", express.urlencoded({extended: false}), router_sign)
 app.use("/login", express.urlencoded({extended: false}), router_login)
+app.use("/modify", express.urlencoded({extended: false}), router_modify)
+
 app.use(express.static("assets"));
 
 http.listen(4911, () => {
